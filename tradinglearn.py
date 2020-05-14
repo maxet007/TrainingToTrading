2099818b4c9512bd2d8ad71900578a4d4fd84067
maxet007 <cyberglebov@gmail.com>
2020-05-14 13:17:02 +0300

    Version0.1

--- tradinglearn.py
+++ tradinglearn.py
@@ -62,13 +62,13 @@ def getPriceToLabel():
 
 
 def getPrice(send):
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36'}
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
     url = 'https://www.google.com/search?q=акция ' + send
     r = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(r.content, 'html.parser')
     #price
-    convert = soup.findAll("span", {"class":"IsqQVc NprOob"})
+    convert = soup.findAll("span", {"class":"IsqQVc NprOob XcVN5d"})
     #name of share
     acName = soup.findAll("div", {"class":"oPhL2e"})
     
